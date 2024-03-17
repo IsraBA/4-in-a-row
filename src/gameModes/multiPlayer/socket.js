5b73026a38cf43eff18dbe329f15a1d98b9f573e
IsraBA <IsraelBenAri1000@gmail.com>
2024-03-17 13:09:21 +0200

    prodaction

--- src/gameModes/multiPlayer/socket.js
+++ src/gameModes/multiPlayer/socket.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+let prodaction = true;
+
+const URL = prodaction ? 'https://four-in-a-row-server-1.onrender.com' : 'http://localhost:3001';
 
 export const socket = io(URL);
