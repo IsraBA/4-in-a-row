9cc7f8642c9dabd35b1e73e621b5ff008eae5d25
IsraBA <IsraelBenAri1000@gmail.com>
2024-03-19 13:06:23 +0200

    change logo back

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { Suspense, useEffect, useState } from 'react'
 import styles from './App.module.css'
 import Local from './gameModes/Local/Local'
-import OnlinePublic from './gameModes/multiPlayer/OnlinePublic'
-import OnlinePrivate from './gameModes/multiPlayer/OnlinePrivate'
 import { Route, Routes } from 'react-router-dom';
 import OpenScreen from './OpenScreen'
 
-// const OnlinePublic = React.lazy(() => import('./gameModes/multiPlayer/OnlinePublic/index'))
-// const OnlinePrivate = React.lazy(() => import('./gameModes/multiPlayer/OnlinePrivate/index'))
+const OnlinePublic = React.lazy(() => import('./gameModes/multiPlayer/OnlinePublic/index'))
+const OnlinePrivate = React.lazy(() => import('./gameModes/multiPlayer/OnlinePrivate/index'))
 
 function App() {
 
@@ -16,17 +14,17 @@ function App() {
       <Routes>
         <Route path='/' element={<OpenScreen />} />
         <Route path='/onlinePublic' element={
-          // <Suspense fallback={<div>טוען...</div>}></Suspense>
-          <OnlinePublic />
-        } />
+          <Suspense fallback={<div>טוען...</div>}>
+            <OnlinePublic />
+          </Suspense>} />
         <Route path='/onlinePrivate' element={
-          // <Suspense fallback={<div>טוען...</div>}></Suspense>
-          <OnlinePrivate />
-        } />
+          <Suspense fallback={<div>טוען...</div>}>
+            <OnlinePrivate />
+          </Suspense>} />
         <Route path='/onlinePrivate/:privateRoomId' element={
-          // <Suspense fallback={<div>טוען...</div>}></Suspense>
-          <OnlinePrivate />
-        } />
+          <Suspense fallback={<div>טוען...</div>}>
+            <OnlinePrivate />
+          </Suspense>} />
         <Route path='/local' element={<Local />} />
       </Routes>
     </div>
