62f37355163c75eb9e7433e4be2fc80480592515
IsraBA <IsraelBenAri1000@gmail.com>
2024-03-19 13:00:51 +0200

    try fix private bug #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Suspense, useEffect, useState } from 'react'
 import styles from './App.module.css'
 import Local from './gameModes/Local/Local'
+import OnlinePublic from './gameModes/multiPlayer/OnlinePublic'
+import OnlinePrivate from './gameModes/multiPlayer/OnlinePrivate'
 import { Route, Routes } from 'react-router-dom';
 import OpenScreen from './OpenScreen'
 
-const OnlinePublic = React.lazy(() => import('./gameModes/multiPlayer/OnlinePublic/index'))
-const OnlinePrivate = React.lazy(() => import('./gameModes/multiPlayer/OnlinePrivate/index'))
+// const OnlinePublic = React.lazy(() => import('./gameModes/multiPlayer/OnlinePublic/index'))
+// const OnlinePrivate = React.lazy(() => import('./gameModes/multiPlayer/OnlinePrivate/index'))
 
 function App() {
 
@@ -14,17 +16,17 @@ function App() {
       <Routes>
         <Route path='/' element={<OpenScreen />} />
         <Route path='/onlinePublic' element={
-          <Suspense fallback={<div>טוען...</div>}>
-            <OnlinePublic />
-          </Suspense>} />
+          // <Suspense fallback={<div>טוען...</div>}></Suspense>
+          <OnlinePublic />
+        } />
         <Route path='/onlinePrivate' element={
-          <Suspense fallback={<div>טוען...</div>}>
-            <OnlinePrivate />
-          </Suspense>} />
+          // <Suspense fallback={<div>טוען...</div>}></Suspense>
+          <OnlinePrivate />
+        } />
         <Route path='/onlinePrivate/:privateRoomId' element={
-          <Suspense fallback={<div>טוען...</div>}>
-            <OnlinePrivate />
-          </Suspense>} />
+          // <Suspense fallback={<div>טוען...</div>}></Suspense>
+          <OnlinePrivate />
+        } />
         <Route path='/local' element={<Local />} />
       </Routes>
     </div>
