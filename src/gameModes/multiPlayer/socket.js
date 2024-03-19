3a6fbcb49be5a32ee2f5a95a73d5708e86ada826
IsraBA <IsraelBenAri1000@gmail.com>
2024-03-19 13:14:06 +0200

    change logo back

--- src/gameModes/multiPlayer/socket.js
+++ src/gameModes/multiPlayer/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-let prodaction = false;
+let prodaction = true;
 
 const URL = prodaction ? 'https://four-in-a-row-server-1.onrender.com' : 'http://localhost:3001';
 
